1e87ae1f4f93a33c190b7f8f85f191a2fbee4afa
nicedavid98 <nicedavid98@daum.net>
2024-10-28 20:28:32 +0900

    쓸모없는 주석 및 코드 제거

--- src/context/ProfileContext.js
+++ src/context/ProfileContext.js
@@ -1,33 +1,3 @@
-// import React, { createContext, useState, useContext } from 'react';
-
-// // 프로필 상태를 위한 Context 생성
-// const ProfileContext = createContext();
-
-// // 프로필 상태를 관리하는 Provider 컴포넌트
-// export const ProfileProvider = ({ children }) => {
-//   const [profile, setProfile] = useState({}); // 초기값을 빈 객체로 설정
-
-//   // 프로필 업데이트 함수
-//   const updateProfile = (newProfile) => {
-//     setProfile((prevProfile) => ({
-//       ...prevProfile,
-//       ...newProfile, // 새로운 프로필 값을 기존 상태에 병합
-//     }));
-//   };
-
-//   return (
-//     <ProfileContext.Provider value={{ profile, updateProfile }}>
-//       {children}
-//     </ProfileContext.Provider>
-//   );
-// };
-
-// // Context를 사용하기 위한 커스텀 훅
-// export const useProfile = () => useContext(ProfileContext);
-
-
-// ProfileContext.js
-
 import React, { createContext, useState, useContext } from 'react';
 
 const ProfileContext = createContext();
